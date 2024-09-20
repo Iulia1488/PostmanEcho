485c39ecd3d157aa3f67ee8e1375d812de1bf9ae
Julia Bukova <ul4ik17@gmail.com>
2024-09-20 12:52:51 +0300

    commit

---
+++ src/test/java/ru/netology/test/postmanEcho.java
@@ -0,0 +1,25 @@
+package ru.netology.test;
+
+import static io.restassured.RestAssured.authentication;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.jupiter.api.Test;
+
+class postmanEcho {
+
+    @Test
+    void shouldTestSomeData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
